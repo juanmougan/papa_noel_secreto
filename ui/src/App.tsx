4aa1ec805d53f3b76d3afaeeb0b91efcf47d27ee
Juan Manuel Mougan <juanmougan@gmail.com>
2019-12-02 23:17:35 -0300

    Added CSS, wrapped tsx inside a class

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,48 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Pap&aacute; Noel secreto!</h1>
-        <p>
-          <small className="catch-phrase">
-            Same old <span className="italics-name">Secret Santa</span>, but
-            Latinamericanized!
-          </small>
-        </p>
-      </header>
-      <div className="form-container">
-        <form>
-          <div className="form-row">
-            <input
-              type="text"
-              name="gifter"
-              className="gifter-input"
-              placeholder="Add people to the roster"
-              // TODO save value
-              //value={this.state.newItem}
-              //onChange={e => this.updateInput('newItem', e.target.value)}
-            />
-            <button className="gifter-add-btn">+</button>
-          </div>
-          <div className="form-row">
-            <textarea
-              disabled
-              className="roster-preview"
-              placeholder="People currently in the roster"
-            />
-          </div>
-          <div className="form-row">
-            <input type="submit" value="Generate!" />
-          </div>
-        </form>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Pap&aacute; Noel secreto!</h1>
+          <p>
+            <small className="catch-phrase">
+              Same old <span className="italics-name">Secret Santa</span>, but
+              Latinamericanized!
+            </small>
+          </p>
+        </header>
+        <div className="form-container">
+          <form>
+            <div className="form-row">
+              <input
+                type="text"
+                name="gifter"
+                className="gifter-input"
+                placeholder="Add people to the roster"
+                // TODO save value
+                //value={this.state.newItem}
+                //onChange={e => this.updateInput('newItem', e.target.value)}
+              />
+              <button className="gifter-add-btn">+</button>
+            </div>
+            <div className="form-row">
+              <textarea
+                disabled
+                className="roster-preview"
+                placeholder="People currently in the roster"
+              />
+            </div>
+            <div className="form-row">
+              <input type="submit" className="submit-btn" value="Generate!" />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
