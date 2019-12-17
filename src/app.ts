0625287e2177571036246d3192176a0cbc4624ce
Juan Manuel Mougan <juanmougan@gmail.com>
2019-12-17 00:05:38 -0300

    Switched UI to Aurelia

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+interface Person {
+  email: string;
+}
+
+export class App {
+  heading = 'Papa Noel Secreto!';
+  roster: Person[] = [];
+  personEmail = '';
+
+  addPersonToRoster() {
+    if (this.personEmail) {
+      this.roster.push({
+        email: this.personEmail
+      });
+      this.personEmail = '';
+    }
+  }
+
+  removePersonFromRoster(person) {
+    let index = this.roster.indexOf(person);
+    if (index !== -1) {
+      this.roster.splice(index, 1);
+    }
+  }
+}
