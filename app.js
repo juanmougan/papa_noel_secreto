086890b4dcd75ccfdeee48eea14b5d9c3890056b
Juan Manuel Mougan <juanmougan@gmail.com>
2019-12-24 14:02:21 -0300

    env vars

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/send-email', function(req, res) {
+app.post('/rosters', function(req, res) {
   let transporter = nodeMailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
       // should be replaced with real sender's account
-      user: 'hello@gmail.com',
-      pass: 'test'
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS
     }
   });
   let mailOptions = {
     // should be replaced with real recipient's account
-    to: 'info@gmail.com',
+    to: req.body.to,
     subject: req.body.subject,
     text: req.body.message
   };
@@ -33,6 +33,7 @@ app.post('/send-email', function(req, res) {
     }
     console.log('Message %s sent: %s', info.messageId, info.response);
   });
+  // TODO delete this I think
   res.writeHead(301, { Location: 'index.html' });
   res.end();
 });
@@ -41,3 +42,5 @@ let server = app.listen(4000, function() {
   let port = server.address().port;
   console.log('Server started at http://localhost:%s', port);
 });
+
+module.exports = app;
