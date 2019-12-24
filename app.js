2c3481a5198df4020147c84a034114511ccc188a
Juan Manuel Mougan <juanmougan@gmail.com>
2019-12-24 13:26:58 -0300

    App boilerplate

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express'),
+  path = require('path'),
+  nodeMailer = require('nodemailer'),
+  bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let app = express();
 
-var app = express();
+app.use(express.static('public'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.post('/send-email', function(req, res) {
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      // should be replaced with real sender's account
+      user: 'hello@gmail.com',
+      pass: 'test'
+    }
+  });
+  let mailOptions = {
+    // should be replaced with real recipient's account
+    to: 'info@gmail.com',
+    subject: req.body.subject,
+    text: req.body.message
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+  res.writeHead(301, { Location: 'index.html' });
+  res.end();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+let server = app.listen(4000, function() {
+  let port = server.address().port;
+  console.log('Server started at http://localhost:%s', port);
 });
-
-module.exports = app;
