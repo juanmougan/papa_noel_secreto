9976208f81b22c2becd10a9651dc00db0bcb01e5
Juan Manuel Mougan <juanmougan@gmail.com>
2019-12-25 22:54:28 -0300

    First end to end working version! :D

--- app.js
+++ app.js
@@ -10,32 +10,43 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// TODO I need to shuffle the list
 app.post('/rosters', function(req, res) {
+  console.log('Got this req');
+  // TODO change the API, it's parsing a list like this: [ 'first', 'second' ]
+  console.log(req.body);
+  const recepientEmail = req.body[0]; // TODO iterate instead, I'm just getting the first
+  const subject = `You are ${recepientEmail}'s Secret Santa!`;
+  const body = `Don't forget to get a present for ${recepientEmail}`;
   let transporter = nodeMailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-      // should be replaced with real sender's account
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS
     }
   });
+  console.log(`Will send an email to ${recepientEmail}`);
+  console.log(`with subject: ${subject}`);
+  console.log(`and body: ${body}`);
   let mailOptions = {
-    // should be replaced with real recipient's account
-    to: req.body.to,
-    subject: req.body.subject,
-    text: req.body.message
+    // to: req.body.to,
+    to: recepientEmail,
+    subject: subject,
+    // subject: req.body.subject,
+    text: body
+    // text: req.body.message
   };
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      return console.log(error);
+      console.log(error);
+      return res.status(400); // Let's assume all errors are BadRequests
     }
     console.log('Message %s sent: %s', info.messageId, info.response);
   });
-  // TODO delete this I think
-  res.writeHead(301, { Location: 'index.html' });
-  res.end();
+  res.send('""');
+  res.status(201).end();
 });
 
 let server = app.listen(4000, function() {
