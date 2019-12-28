0f16eb6902d5bdc21587bb7921b78640e93a6c19
Juan Manuel Mougan <juanmougan@gmail.com>
2019-12-28 19:22:38 -0300

    Moved mail part to the service

--- app.js
+++ app.js
@@ -9,32 +9,9 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// TODO check "gifter" and "giftee", there's a bug. Because those names SUCK
-async function sendMail(gifterEmail, subject, body) {
-  let nodeMailer = require('nodemailer');
-  let transporter = nodeMailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS
-    }
-  });
-  console.log(`Will send an email to ${gifterEmail}`);
-  console.log(`with subject: ${subject}`);
-  console.log(`and body: ${body}`);
-  let mailOptions = {
-    to: gifterEmail,
-    subject: subject,
-    text: body
-  };
-  let info = await transporter.sendMail(mailOptions);
-  console.log(`info: ${info}`);
-}
-
 app.post('/rosters', async function(req, res) {
   const shuffler = require('./services/shuffle_service');
+  const mailSender = require('./services/mail_service');
   const roster = req.body;
   // TODO change the API, it's parsing a list like this: [ 'first', 'second' ]
   // and it should eventually be {"name": "juan", "juan@mail.com"}
