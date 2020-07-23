9ef9eaa411cab4d64080d6955092c1f1e4465917
Juan Manuel Mougan <juanmougan@gmail.com>
2020-07-23 18:33:13 -0300

    Started working on the form

--- papa_client/src/App.vue
+++ papa_client/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Form />
   </div>
 </template>
+
+<script>
+import Form from "./components/Form.vue";
+export default {
+  name: "app",
+  components: {
+    Form
+  }
+};
+</script>
 
 <style>
 #app {
---
+++ papa_client/src/components/Form.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form-container">
+    <form>
+      <div class="form-row">
+        <!-- TODO maybe this should be a Component, and receive its index via props -->
+        <input
+          type="text"
+          name="gifter"
+          id="gifter-input-id"
+          class="gifter-input"
+          v-model="gifterEmail1"
+          placeholder="Add people to the roster"
+        />
+        <button class="gifter-add-btn" id="gifter-add-btn-id">+</button>
+      </div>
+      <div class="form-row">
+        <ul id="roster-ul">
+          <li>
+            <p>{{ gifterEmail1 }}</p>
+          </li>
+        </ul>
+      </div>
+      <div class="form-row">
+        <input type="submit" class="submit-btn" id="submit-btn-id" value="Generate!" />
+        <input type="reset" id="clear-roster" class="submit-btn" value="Clear" />
+      </div>
+    </form>
+  </div>
+</template>>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  // @Prop() private msg!: string;
+}
+</script>
