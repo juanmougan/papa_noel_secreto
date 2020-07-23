a3e08519e2d024ee9e9a1d3e95b3cfb71bc07866
Juan Manuel Mougan <juan.mougan@roche.com>
2020-07-23 19:24:56 -0300

    Did some progress, but doesn't work yet

--- papa_client/src/components/Form.vue
+++ papa_client/src/components/Form.vue
@@ -8,15 +8,15 @@
           name="gifter"
           id="gifter-input-id"
           class="gifter-input"
-          v-model="gifterEmail1"
+          v-model="gifterEmail[0]"
           placeholder="Add people to the roster"
         />
-        <button class="gifter-add-btn" id="gifter-add-btn-id">+</button>
+        <button class="gifter-add-btn" id="gifter-add-btn-id" v-on:click="gifterEmail.push(gifterEmail[0])">+</button>
       </div>
       <div class="form-row">
         <ul id="roster-ul">
-          <li>
-            <p>{{ gifterEmail1 }}</p>
+          <li v-if="gifterEmail[0]">
+            {{ gifterEmail[0] }}
           </li>
         </ul>
       </div>
@@ -31,8 +31,10 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+const gifterEmail = [];
+
 @Component
-export default class HelloWorld extends Vue {
-  // @Prop() private msg!: string;
+export default class Form extends Vue {
+  private gifterEmail: (string)[] = [];
 }
 </script>
