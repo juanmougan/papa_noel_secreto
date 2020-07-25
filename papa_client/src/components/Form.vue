e8e990df9a9d35401d97f2d9b3b4caf3432059ec
Juan Manuel Mougan <juanmougan@gmail.com>
2020-07-24 23:41:09 -0300

    Adds and removes

--- papa_client/src/components/Form.vue
+++ papa_client/src/components/Form.vue
@@ -3,14 +3,6 @@
     <form>
       <div class="form-row">
         <!-- TODO maybe this should be a Component, and receive its index via props -->
-        <!-- <input
-          type="text"
-          name="gifter"
-          id="gifter-input-id"
-          class="gifter-input"
-          v-bind="someNewGifter"
-          placeholder="Add people to the roster"
-        /> -->
         <input
           type="text"
           name="gifter"
@@ -19,20 +11,19 @@
           v-model="someNewGifter"
           placeholder="Add people to the roster"
         />
-        <!-- <button class="gifter-add-btn" id="gifter-add-btn-id" v-on:click.prevent="gifterEmail.push(gifterEmail[0])">+</button> -->
-        <!-- <button class="gifter-add-btn" id="gifter-add-btn-id" v-on:click.prevent="addGifterEmail(gifterEmail[0])">+</button> -->
-        <button class="gifter-add-btn" id="gifter-add-btn-id" v-on:click.prevent="addGifterEmail(someNewGifter)">+</button>
+        <button class="gifter-add-remove-btn" id="gifter-add-btn-id" v-on:click.prevent="addEmail(someNewGifter, $event)">+</button>
       </div>
       <div class="form-row">
         <ul id="roster-ul">
           <li v-for="gifterEmail in gifterEmails" :key="gifterEmail">
-            {{ gifterEmail }}
+            <span>{{ gifterEmail }}</span>
+            <button class="gifter-add-remove-btn" id="gifter-remove-btn-id" v-on:click.prevent="removeElement(gifterEmail)">-</button>
           </li>
         </ul>
       </div>
       <div class="form-row">
         <input type="submit" class="submit-btn" id="submit-btn-id" value="Generate!" />
-        <input type="reset" id="clear-roster" class="submit-btn" value="Clear" />
+        <input type="reset" id="clear-roster" class="submit-btn" value="Clear" v-on:click.prevent="removeAllElements()" />
       </div>
     </form>
   </div>
@@ -41,19 +32,35 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
-// const gifterEmail = [];
-
 @Component
 export default class Form extends Vue {
   private gifterEmails: string[] = [];
   private someNewGifter = '';
 
-  // TODO this gets erased almost immediately
-  // Maybe I need props?
-  addGifterEmail(gifter: string) {
+  addEmail(email: string, event) {
+    this.addElement(email);
+    this.someNewGifter = '';
+    event.preventDefault();
+  }
+
+  // TODO maybe a Set would be better
+  addElement(gifter: string) {
     console.log(`Received gifter: ${gifter}`);
     this.gifterEmails.push(gifter);
     console.log(`Now the array is: ${this.gifterEmails}`);
+  }
+
+  removeElement(gifter: string) {
+    console.log(`Will remove gifter: ${gifter}`);
+    const index = this.gifterEmails.indexOf(gifter);
+    if (index > -1) {
+      this.gifterEmails.splice(index, 1);
+    }
+    console.log(`Now the array is: ${this.gifterEmails}`);
+  }
+
+  removeAllElements() {
+    this.gifterEmails = [];
   }
 
 }
