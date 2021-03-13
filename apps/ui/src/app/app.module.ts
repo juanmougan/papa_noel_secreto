291c7497f2ef395d71a2d9ad9581c27479554ac5
Juan Manuel Mougan <juanmougan@gmail.com>
2021-03-13 16:26:32 -0300

    feat: add and remove from the list

--- apps/ui/src/app/app.module.ts
+++ apps/ui/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RosterComponent } from './roster/roster.component';
 
 @NgModule({
   declarations: [AppComponent, RosterComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/ui/src/app/roster/roster.component.ts
+++ apps/ui/src/app/roster/roster.component.ts
@@ -3,15 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'papa-noel-secreto-roster',
   templateUrl: './roster.component.html',
-  styleUrls: ['./roster.component.css']
+  styleUrls: ['./roster.component.css'],
 })
 export class RosterComponent implements OnInit {
+  gifters: Array<string>;
+  newGifter: string;
 
-  gifters: Array<string>
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.gifters = [];
+    this.newGifter = '';
   }
 
+  addGifter() {
+    this.gifters.push(this.newGifter);
+    this.newGifter = '';
+  }
+
+  removeGifter(index: number) {
+    this.gifters.splice(index, 1);
+  }
+
+  onSubmit() {}
 }
